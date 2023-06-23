c2cfa692b898fd80222532aef965f6e55505add5
Santosh Narayanan <santosh33@gmail.com>
2023-06-23 18:32:50 +0200

    class component flow

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: []
     };
+    console.log('1');
   }
 
   componentDidMount() {
+    console.log('3');
     fetch('http://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(() => {
@@ -25,6 +27,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('2');
     return (
       <div className="App">
         {
