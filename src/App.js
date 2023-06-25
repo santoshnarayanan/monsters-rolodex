08589b3886f60141fb63cfc56c3cf24359a67c34
Santosh Narayanan <santosh33@gmail.com>
2023-06-25 13:24:32 +0200

    Card-List component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 //import logo from './logo.svg';
 import './App.css';
 // import { render } from '@testing-library/react';
@@ -49,13 +50,14 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters'
+        {/* <input className='search-box' type='search' placeholder='search monsters'
           onChange={onSearchChange} />
         {
           filteredMonsters.map((monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>;
           })
-        }
+        } */}
+        <CardList />
       </div>
     );
   };
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CardList extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello I am the card-list component</h1>
+            </div>
+        );
+    }
+}
+
+export default CardList;
