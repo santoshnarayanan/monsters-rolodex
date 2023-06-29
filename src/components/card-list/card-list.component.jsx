7a9550cc27f3d65f76ecc93f9f967620b1a1016c
Santosh Narayanan <santosh33@gmail.com>
2023-06-30 00:58:28 +0200

    CardList-Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,22 @@
 import { Component } from "react";
-
+import './card-list.styles.css';
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
+
         return (
-            <div>
+            <div className='card-list'>
                 {monsters.map((monster) => {
-                    return <h3 key={monster.id}>{monster.name}</h3>
+                    const { name, email, id } = monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <img alt={`monster ${monster.name}`}
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                            />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    );
                 })}
             </div>
         );
